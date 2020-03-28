979bc4ac12ec43e3a18cba01276c5067c97d8933
Andres <andresnrx@gmail.com>
2020-03-27 21:26:50 -0700

    fork & exec

--- tag/src/process.rs
+++ tag/src/process.rs
@@ -4,31 +4,64 @@ use crate::{
     ExecNameError,
 };
 use chrono::Utc;
-use nix::unistd;
+use nix::{sys, unistd};
+use std::ffi::CString;
 
-pub fn process(_: bool) -> Result<Vec<Log>, Error> {
+pub fn process(exec: bool) -> Result<Vec<Log>, Error> {
     let command_line = std::env::args().collect::<Vec<String>>().join(" ");
 
     let fork = unistd::fork().map_err(|e| Error::WaitChild(assume_errno(e)))?;
 
     match fork {
-        unistd::ForkResult::Child => std::process::exit(0),
+        unistd::ForkResult::Child => {
+            if exec {
+                let ls = CString::new("/usr/bin/printf").expect("CString::new failed");
+
+                unistd::execv(
+                    &ls,
+                    &[
+                        &&ls,
+                        &&CString::new("").unwrap(),
+                    ],
+                )
+                .expect("failed to execute");
+                unreachable!();
+            } else {
+                std::process::exit(0)
+            }
+        }
         unistd::ForkResult::Parent { child } => {
-            let _ = nix::sys::wait::waitpid(child, None)
-                .map_err(|e| Error::WaitChild(assume_errno(e)))?;
+            let child_satus =
+                sys::wait::waitpid(child, None).map_err(|e| Error::WaitChild(assume_errno(e)))?;
 
-            let log = Log {
+            let username = whoami::username();
+            let parent_pid = std::process::id();
+            let child_pid = i32::from(child) as u32;
+
+            let mut logs = vec![Log {
                 command_line,
+                username: username.clone(),
                 time: Utc::now(),
-                username: whoami::username(),
-                pid: std::process::id(),
+                pid: parent_pid,
                 process_name: current_process_name().map_err(Error::ExecName)?,
-                activity: ActivityLog::ProcessFork {
-                    child_pid: i32::from(child) as u32,
-                },
-            };
+                activity: ActivityLog::ProcessFork { child_pid },
+            }];
 
-            Ok(vec![log])
+            if exec {
+                match child_satus {
+                    sys::wait::WaitStatus::Exited(_, 0) => logs.push(Log {
+                        username,
+                        command_line: "/usr/bin/printf ''".into(),
+                        time: Utc::now(),
+                        pid: child_pid,
+                        process_name: "printf".into(),
+                        activity: ActivityLog::ProcessExec { parent_pid },
+                    }),
+                    _ => return Err(Error::ChildExit),
+                }
+            }
+
+            Ok(logs)
         }
     }
 }
@@ -47,4 +80,5 @@ pub enum Error {
     Fork(nix::errno::Errno),
     WaitChild(nix::errno::Errno),
     ExecName(ExecNameError),
+    ChildExit,
 }
